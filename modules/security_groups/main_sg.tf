2d6b8c4bbf33ed46403b4a40ec94c73df039ae8f
Kashinath22 <borkerkashinath@gmail.com>
2023-03-20 13:00:16 +0530

    added private security group

--- modules/security_groups/main_sg.tf
+++ modules/security_groups/main_sg.tf
@@ -18,23 +18,23 @@ resource "aws_security_group" "k-public-sg" {
   }
 }
 
-# resource "aws_security_group" "k-private-sg" {
-#   name        = "${var.k-pvt-sg-name}"
-#   description = "${var.k-desription-pvt}"
-#   vpc_id = var.kvpcid
+resource "aws_security_group" "k-private-sg" {
+  name        = "${var.k-pvt-sg-name}"
+  description = "${var.k-desription-pvt}"
+  vpc_id = var.kvpcid
 
-#   ingress {
+  ingress {
 
-#     to_port     = var.pvt-ing-port
-#     from_port   = var.pvt-ing-port
-#     protocol    = "${var.pvt-ing-protocol}"
-#     cidr_blocks = [var.pvt-ing-cidr]
-#   }
-#   egress {
+    to_port     = var.pvt-ing-port
+    from_port   = var.pvt-ing-port
+    protocol    = "${var.pvt-ing-protocol}"
+    cidr_blocks = [var.pvt-ing-cidr]
+  }
+  egress {
 
-#     to_port     = var.pvt-egr-port
-#     from_port   = var.pvt-egr-port
-#     protocol    =  "${var.pvt-egr-protocol}"
-#     cidr_blocks = [var.pvt-egr-cidr]
-#   }
-# }
+    to_port     = var.pvt-egr-port
+    from_port   = var.pvt-egr-port
+    protocol    =  "${var.pvt-egr-protocol}"
+    cidr_blocks = [var.pvt-egr-cidr]
+  }
+}
